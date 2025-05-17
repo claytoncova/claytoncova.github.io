56195fdba54a569f3faa0ebe55500437450122a2
claytoncova <cgcs@protonmail.com>
2025-05-17 15:03:52 -0400

    Insert Mario

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,4 +62,76 @@ document.addEventListener('DOMContentLoaded', () => {
             setupMobileNav();
         }
     });
+
+    //Animação do Mario
+    var player = document.getElementById('mario')
+    var powerup = {
+    audio: new Audio('http://themushroomkingdom.net/sounds/wav/smb/smb_powerup.wav'),
+    play: function() { 
+        this.audio.currentTime = 0;
+        this.audio.play() 
+    }
+    }
+
+    // Local do Mario
+    function updateElement(element, incx, incy) {
+    var x = Number(element.getAttribute('data-x')) + incx
+    var y = Number(element.getAttribute('data-y')) + incy
+    
+    /*
+    // Ejemplo básico de límite de regiões
+    if ((x < 0) || (x > 600)) 
+        return
+    */
+    
+    element.style.transform = 'translate('+ x +'px, '+ y +'px)'
+    
+    if (element.classList.contains('mirror'))
+        element.style.transform += ' scaleX(-1)'
+        
+    if (element.classList.contains('big'))
+        element.style.transform += ' scale(2)'
+    
+    // Update HTML
+    element.setAttribute('data-x', x)
+    element.setAttribute('data-y', y)
+    }
+
+    // Aperta a tecla
+    window.addEventListener('keydown', function(e) {
+    console.log(e)
+    
+    var speed = 10;
+    //var speed = (e.ctrlKey ? 20 : 10)
+    
+    // Direita
+    if (e.keyCode == 39) {
+        player.classList.add('caminar')
+        player.classList.remove('mirror')
+        updateElement(player, +speed, 0)
+    }
+    // Esquerda
+    else if (e.keyCode == 37) {
+        player.classList.add('caminar')
+        player.classList.add('mirror')
+        updateElement(player, -speed, 0)
+    }
+    
+    if (e.keyCode == 85) {
+        player.classList.toggle('big')
+        //new Audio('http://themushroomkingdom.net/sounds/wav/smb/smb_powerup.wav').play()
+        powerup.play(); // fix network lag sound    
+        updateElement(player, 0, 0)
+    }
+    
+    })
+
+    // solta tecla
+    window.addEventListener('keyup', function(e) {
+    // retorna ao primeira imagem
+    player.classList.remove('caminar')
+    })
+
+    // Inicialização
+    updateElement(player, 0, 0)
 }); 
